bbb7b045ffedc559de1a0cd7ae18233cb5eef024
Marcos <m.vinicius.sr11@gmail.com>
2023-08-29 11:42:07 -0300

    primeiro commit

---
+++ BuildScript/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using FlubuCore.Context;
+using FlubuCore.Scripting;
+using System;
+using FlubuCore.Context.Attributes.BuildProperties;
+
+namespace BuildScript
+{
+    public class MyBuildScript : DefaultBuildScript
+{   
+    protected override void ConfigureTargets(ITaskContext context)
+    {
+        var compile = context.CreateTarget("compile")
+            .SetDescription("Compiles the solution.")
+            .SetAsDefault()
+            .AddCoreTask(x => x.Build("TestandoFlubuCore.sln"));
+    }
+}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestandoFlubuCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
