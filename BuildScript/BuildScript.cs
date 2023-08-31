b8076d0868d109874e1d594c014a918b27e0db14
Marcos <m.vinicius.sr11@gmail.com>
2023-08-31 11:53:49 -0300

    .

--- BuildScript/BuildScript.cs
+++ BuildScript/BuildScript.cs
@@ -5,6 +5,7 @@ using System;
 using FlubuCore.Context.Attributes.BuildProperties;
 using FlubuCore;
 using FlubuCore.Targeting;
+using FlubuCore.Context.FluentInterface.TaskExtensions;
 
 namespace BuildScript
 {
@@ -15,14 +16,28 @@ namespace BuildScript
         var compile = context.CreateTarget("compile")
             .SetDescription("Compiles the solution.")
             .SetAsDefault()
-            .AddCoreTask(x => x.Build("TestandoFlubuCore.sln"))
-            .AddCoreTask(x => x.Pack())
-            .AddCoreTask(x => x.NugetPush("C:\\work\\TestandoFlubuCore\\bin\\Debug"))
-            ;
+            .AddCoreTask(x => x.Build("TestandoFlubuCore.sln"));
 
+        var publicar = context.CreateTarget("publish")
+                .SetDescription("Publish the solution")
+                .AddCoreTask(x => x.Publish("TestandoFlubuCore.sln", "C:\\work\\TestandoFlubuCore", "release"));
             
 
-           
+        var nuGetpackage = context.CreateTarget("nuGetPack")
+                .SetDescription("Create a NuGet Package")
+                .AddCoreTask(x => x.Pack());
+            // .AddCoreTask(x => x.NugetPush("C:\\work\\TestandoFlubuCore\\bin\\Debug"));
+               
+                
+
+        var zipPackage = context.CreateTarget("zipPack")
+                .SetDescription("creating a zip package")
+                .AddCoreTask(x => x.CreateZipPackageFromProjects("zipFileName.zip", "myFramework", ""));
+
+
+
+
+
     }
 
       
